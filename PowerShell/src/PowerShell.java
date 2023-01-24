87f643bf3f3049cdef2d0d37c4db87ca326f4b96
Wallace Ricardo Pereira Junior <102304787+WallaceRPJr@users.noreply.github.com>
2023-01-24 20:20:58 -0300

    teste

--- PowerShell/src/PowerShell.java
+++ PowerShell/src/PowerShell.java
@@ -1,12 +1,53 @@
+import java.io.File;
+import java.util.Properties;
 import java.util.Scanner;
+
+// pasta dos arquivos : C:\\Users\\Cliente\\OneDrive\\Documentos\\ProjetosGit\\PowerShell\\Diretorios
 
 public class PowerShell {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
-        String a = " ";
 
-        while(a != "sair"){
-             
+
+        String a = " ";
+        String x , y;
+
+
+        while(!a.equals("sair")){
+            System.out.print("/>"); 
+            a = in.nextLine();
+
+            if(a.equals("ver")){
+                System.out.println( System.getProperty("os.name") );
+            }
+
+            if(a.equals("mv")){
+                System.out.print("/>mv ");
+                x = in.nextLine();
+                if(x.equals("exit")) return;
+
+                System.out.print("/>mv " + x + " ");
+                y = in.nextLine();
+                if(y.equals("exit")) return;
+
+            }
+
+            if(a.equals("mkdir")){
+                System.out.print("/>mkdir ");
+                x = in.nextLine();
+
+                if(x.equals("exit")) return;
+
+                java.io.File diretorio = new java.io.File("C:\\Users\\Cliente\\OneDrive\\Documentos\\ProjetosGit\\PowerShell\\Diretorios\\" + x); 
+
+                boolean statusDir = diretorio.mkdir();
+                System.out.print(statusDir); 
+
+
+            }
+
+
+
         }
 
     }
