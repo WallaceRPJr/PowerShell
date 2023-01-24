49803fa0b017d4a5e954753cccfc43811605944c
Wallace Ricardo Pereira Junior <102304787+WallaceRPJr@users.noreply.github.com>
2023-01-24 19:35:19 -0300

    FirstCommit

---
+++ PowerShell/src/PowerShell.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class PowerShell {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        String a = " ";
+
+        while(a != "sair"){
+             
+        }
+
+    }
+}
